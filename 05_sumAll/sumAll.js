5acd7500951b6a524c7fa006ae951bb2aa6e6fb4
guillaumegiannantonio <nesanco3@gmail.com>
2023-06-24 22:45:56 +0200

    Finish exercice 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(numberDown, numberUp) {
+	if(typeof numberDown !== 'number' || typeof numberUp !== 'number' || numberDown < 0 || numberUp < 0) {
+		return 'ERROR'
+	}
+	if(numberUp < numberDown) {
+		let temp = numberUp
+		numberUp = numberDown
+		numberDown = temp
+	}
+	let result = 0
+	for (let i = numberDown; i <= numberUp; i++) {
+		result += i
+	}
+	return result
 };
 
 // Do not edit below this line
