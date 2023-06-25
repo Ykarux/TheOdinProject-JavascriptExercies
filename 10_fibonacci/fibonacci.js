8a00414d9f4f8ee77b4d0e1587812d0f0036b173
guillaumegiannantonio <nesanco3@gmail.com>
2023-06-25 15:22:13 +0200

    Finish exercice 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+	if(a < 0) {
+		return "OOPS"
+	}
+	if(typeof a === 'string') {
+		a = Number(a)
+	}
+	let x = 1
+	let y = 2
+	for (let i = 1; i < a - 1; i++) {
+		let temp = y
+		y = y + x
+		x = temp
+	}
+	return x
 };
 
 // Do not edit below this line
