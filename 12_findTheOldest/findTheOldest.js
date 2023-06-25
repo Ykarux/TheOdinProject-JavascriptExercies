a33d1824fc8be90c77574164aeb9c2947580f10e
guillaumegiannantonio <nesanco3@gmail.com>
2023-06-25 17:21:57 +0200

    Finish exercice 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+	return arr.reduce((oldest, element) => {
+		if(element.yearOfDeath === undefined) {
+			if ((2023 - element.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth)) {
+				return element
+			} else {
+				return oldest
+			}
+		}
+		if ((element.yearOfDeath - element.yearOfBirth) > (oldest.yearOfDeath - oldest.yearOfBirth)) {
+			return element
+		} else {
+			return oldest
+		}
+	}, arr[1])
 };
 
 // Do not edit below this line
