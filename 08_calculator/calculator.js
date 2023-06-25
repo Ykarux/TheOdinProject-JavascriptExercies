b89a39f82787b39338327759021cce761b6866f8
guillaumegiannantonio <nesanco3@gmail.com>
2023-06-25 14:58:36 +0200

    Finish exercice 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, value) => total + value, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+    return args.reduce((total, value) => total * value, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let result = 1;
+
+	for (let i = 0; i < b; i++) {
+		result *= a;
+	}
+
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a === 0) {
+		return 1
+	}
+    if(a > 1) {
+      return a * factorial(a-1)
+    }
+	return a
 };
 
 // Do not edit below this line
