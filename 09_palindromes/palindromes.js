2e07aec84786a145831610128b815d609e41fdc4
guillaumegiannantonio <nesanco3@gmail.com>
2023-06-25 15:12:06 +0200

    Finish exercice 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+	string = string.toLowerCase()
+	string = string.replace(/\s/g, '');
+	for (let i = 0; i < string.length; i++) {
+		if(string.charAt(string.length - i - 1) !== string.charAt(i)) {
+			return false
+		}
+	}
+	return true
 };
 
 // Do not edit below this line
